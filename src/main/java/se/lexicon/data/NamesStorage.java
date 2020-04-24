603daea474470fc6a415683ff35dd32718440ff9
Erik Svensson <eosvensson@gmail.com>
2020-04-24 09:42:02 +0200

    Added nameExists() in NamesStorage

--- src/main/java/se/lexicon/data/NamesStorage.java
+++ src/main/java/se/lexicon/data/NamesStorage.java
@@ -4,9 +4,26 @@ public class NamesStorage {
 
     private static String[] names = new String[0];
 
+    /**
+     *
+     * @param fullName String fullName
+     * @return true if found
+     */
+    public static boolean nameExists(final String fullName){
+        boolean exists = false;
+        for(String name : names){
+            if(name.equalsIgnoreCase(fullName)){
+                exists = true;
+                break; //Breaks out of a loop or a switch.
+            }
+        }
+        return exists;
+    }
 
 
-
+    /**
+     * Empties the array by overwriting it with a new empty array
+     */
     public static void clear(){
         names = new String[0];
     }
